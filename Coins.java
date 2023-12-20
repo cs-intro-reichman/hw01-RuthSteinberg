992f6d9d63ec8335de2582b82e9b5fb7315e36c8
RuthSteinberg <154434437+RuthSteinberg@users.noreply.github.com>
2023-12-20 14:37:22 +0200

    Update Coins.java
    Upload Coins.java

--- Coins.java
+++ Coins.java
@@ -4,6 +4,13 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		//get number of cents from the command-line and define him as a variable of type int
+	        int sumCents = Integer.parseInt(args[0]);
+	        // calculate the amount of quarters 
+	        int quarters = sumCents / 25;
+	        // calculate the remaining cents
+	        int cents = sumCents - (quarters*25);
+	        // print 
+	        System.err.println("Use " + quarters + " quarters and " + cents + " cents");
 	}
 }
