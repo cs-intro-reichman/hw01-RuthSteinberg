ea7ff30c024365988f4fda6b424016811853465f
RuthSteinberg <154434437+RuthSteinberg@users.noreply.github.com>
2023-12-21 22:15:44 +0200

    Update Triangle.java
    change (boolean instead if)/

--- Triangle.java
+++ Triangle.java
@@ -5,18 +5,15 @@
  */ 
 public class Triangle {
     public static void main(String[] args) {
-        //get 3 numbers from the command-line and define them as a variable of type int
+        //gets 3 numbers from the command-line and define them as a variable of type int
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
-        // define a boolean variable that represent the answaer (if the 3 numbers can form triangle)
-        boolean answer = false;
+        // define a boolean variable that represent the answaer (if the 3 numbers can form triangle) 
         // check if any two sides is greater than the length of the remaining side
-        if(((a+b)>=c) && ((b+c)>=a) && ((a+c)>=b)) {
-            answer = true;
-        }
+        boolean answer = (((a+b)>c) && ((b+c)>a) && ((a+c)>b));
         // print the 3 numbers and the answer
-        System.out.println(a + ", " + b + ", " + c + ": " + answer); 
+        System.out.println(a + " , " + b + " , " + c + ": " + answer); 
     }
 }
 
